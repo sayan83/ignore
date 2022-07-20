50535d6513c09c457c7319a1718d4901b363d7e1
Harsh-kumar-sinha <harshsinha1999@gmail.com>
2022-07-20 22:33:35 +0530

    Lambda,filter and Map functions used

--- sample.py
+++ sample.py
@@ -1,2 +1,14 @@
 for i in range(3,10):
     print(i,"Hello "+str(i))
+
+val = lambda x : x + 10
+arr = [10,20,30,40,51,78,99,21]
+
+check_even = list(filter(lambda x : x%2==0,arr))
+
+check_odd = list(map(lambda x : x%2!=0,arr))
+
+print("The answer is",val(15))
+
+print(check_even)
+print(check_odd)
